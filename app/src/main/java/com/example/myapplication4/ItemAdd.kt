4f718a1b5b20583ddb099e98a75f890e23a76aaf
Annie.ch <diacy710902@gmail.com>
2021-06-22 02:50:15 +0800

    add item

--- app/src/main/java/com/example/myapplication4/ItemAdd.kt
+++ app/src/main/java/com/example/myapplication4/ItemAdd.kt
@@ -2,10 +2,44 @@ package com.example.myapplication4
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.AdapterView
+import android.widget.ArrayAdapter
+import android.widget.Toast
+import android.widget.*
 
 class ItemAdd : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_item_add)
+
+        val location = resources.getStringArray(R.array.Location)
+        val spinner = findViewById<Spinner>(R.id.spinner)
+        if (spinner != null) {
+            val adapter = ArrayAdapter(
+                this,
+                android.R.layout.simple_spinner_item, location
+            )
+            spinner.adapter = adapter
+
+            spinner.onItemSelectedListener = object :
+                AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>,
+                    view: View, position: Int, id: Long
+                ) {
+                    Toast.makeText(
+                        this@ItemAdd,
+                        getString(R.string.selected_item) + " " +
+                                "" + location[position], Toast.LENGTH_SHORT
+                    ).show()
+                }
+
+                override fun onNothingSelected(parent: AdapterView<*>) {
+                    // write code to perform some action
+                }
+            }
+        }
+
     }
 }
